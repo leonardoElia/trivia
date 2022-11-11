8f38146e2605507b469f168eaf52f819a2dd24eb
Ivan <ivan.dadalto@gmail.com>
2022-11-11 18:35:57 -0300

    req8

--- src/componentes/Timer.js
+++ src/componentes/Timer.js
@@ -1,59 +1,8 @@
 import React from 'react';
-// const thirty = 30;
 
 class Timer extends React.Component {
-  state = {
-    timer: 30,
-    // waitTimer: true,
-  };
-
-  // handleFiveTime = () => {
-  //   this.setState({
-  //     waitTimer: true,
-  //   });
-  //   const FIVE = 5000;
-  //   setTimeout(() => {
-  //     this.setState({
-  //       waitTimer: false,
-  //     });
-  //   }, FIVE);
-  // };
-
-  componentDidMount() {
-    const ONE_SECOND = 1000;
-    this.intervalID = setInterval(() => {
-      this.setState((prev) => ({
-        timer: prev.timer - 1,
-      }));
-    }, ONE_SECOND);
-  }
-
-  // shouldComponentUpdate(_nextProps, nextState) {
-  //   const { timer } = nextState;
-  //   return (timer !== 0);
-  // }
-
-  componentDidUpdate(_prevProps, prevState) {
-    if (prevState.timer === 1) {
-      clearInterval(this.intervalID);
-    }
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-    console.log('Timer Desmontado');
-  }
-
-  // handleTime = () => {
-  //   setInterval(() => {
-  //     this.setState((prev) => ({
-  //       timer: prev.timer - 1,
-  //     }));
-  //   }, ONE_SECOND);
-  // };
-
   render() {
-    const { timer } = this.state;
+    const { timer } = this.props;
     return (
       <h2>
         { timer }
@@ -62,4 +11,7 @@ class Timer extends React.Component {
   }
 }
 
+Timer.propTypes = {
+  timer: PropTypes.number.isRequired,
+};
 export default Timer;
