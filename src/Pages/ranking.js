fafa3c7678c6eb21b7bb26c8de5a2133eacd6b8f
Ivan <ivan.dadalto@gmail.com>
2022-11-14 17:54:27 -0300

    resolve req 18

--- src/Pages/ranking.js
+++ src/Pages/ranking.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  btnGoHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.btnGoHome }
+        >
+          Back to login
+        </button>
+      </div>
     );
   }
 }
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
